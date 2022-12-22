6542db598f6c2a66d3ec0502172b75dfb87dd9d6
Ansh Rajpoot <109946926+AnshRajpoot@users.noreply.github.com>
2022-12-22 14:56:08 +0530

    Update average.py

--- average.py
+++ average.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
 #read csv file
+from google.colab import files
+import io
+uploaded = files.upload()
+df1 = pd.read_csv((uploaded['iris.csv']))
+df1
 #find the average of petal width
+pw_avg=pw_mean/pw_len;
 #round off to 2 decimal places
+print(round(pw_avg,2))
 #print only average
+print(pw_avg)
